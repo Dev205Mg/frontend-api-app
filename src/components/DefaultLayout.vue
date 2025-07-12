0e83e83effbd478dda031550ebccaa0883c387f6
Dev205.mg <dev205mg@gamil.com>
2025-07-12 09:55:03 +0300

    edit user in home for only authantificate

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -20,7 +20,7 @@
               </div>
             </div>
           </div>
-          <div class="hidden md:block">
+          <div v-if="user" class="hidden md:block">
             <div class="ml-4 flex items-center md:ml-6">
 
               <!-- Profile dropdown -->
@@ -30,7 +30,7 @@
                     class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
                     <span class="absolute -inset-1.5" />
                     <span class="sr-only">Open user menu</span>
-                    <img class="size-8 rounded-full" :src="user.imageUrl" alt="" />
+                    <img class="size-8 rounded-full" :src="userAuth.imageUrl" alt="" />
                   </MenuButton>
                 </div>
                 <transition enter-active-class="transition ease-out duration-100"
@@ -49,7 +49,7 @@
               </Menu>
             </div>
           </div>
-          <div class="-mr-2 flex md:hidden">
+          <div v-if="user" class="-mr-2 flex md:hidden">
             <!-- Mobile menu button -->
             <DisclosureButton
               class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
@@ -62,7 +62,7 @@
         </div>
       </div>
 
-      <DisclosurePanel class="md:hidden">
+      <DisclosurePanel v-if="user" class="md:hidden">
         <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
           <RouterLink v-for="nav in navigation" :key="nav.name" :to="nav.to"
             :class="[
@@ -73,11 +73,11 @@
         <div class="border-t border-gray-700 pt-4 pb-3">
           <div class="flex items-center px-5">
             <div class="shrink-0">
-              <img class="size-10 rounded-full" :src="user.imageUrl" alt="" />
+              <img class="size-10 rounded-full" :src="userAuth.imageUrl" alt="" />
             </div>
             <div class="ml-3">
-              <div class="text-base/5 font-medium text-white">{{ user.name }}</div>
-              <div class="text-sm font-medium text-gray-400">{{ user.email }}</div>
+              <div class="text-base/5 font-medium text-white">{{ userAuth.name }}</div>
+              <div class="text-sm font-medium text-gray-400">{{ userAuth.email }}</div>
             </div>
           </div>
           <div class="mt-3 space-y-1 px-2">
@@ -99,9 +99,12 @@ import { useAuthStore } from '@/stores/auth'
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
+
+const { logout, user } = useAuthStore()
+
+const userAuth = {
+  name: user?.name,
+  email: user?.email,
   imageUrl:
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
@@ -110,5 +113,4 @@ const navigation = [
   { name: 'Dashboard', to: { name: 'dashboard' } },
 ]
 
-const { logout } = useAuthStore()
 </script>
